633133bbebb6f9ec0e3d93daeb77b9420d7f6704
SaidMO <saeedos@gmail.com>
2023-05-02 22:47:07 +0200

    Fallback controller added, BookModel added, migration add, fallback view added

---
+++ resources/views/fallback/index.blade.php
@@ -0,0 +1,3 @@
+<h1>
+    Fallback page once a route dosn't exist
+</h1>
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,11 @@
 <?php
 
 use App\Http\Controllers\BooksController;
+use App\Http\Controllers\FallbackController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/', [BooksController::class,'index']);
 
-Route::get('/', [BooksController::class,'index'])->name('home');
+// Fallback Controller
+// Check Fallbackcontroller and Fallback view
+Route::fallback(FallbackController::class);
